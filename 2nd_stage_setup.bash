e1f7dfbf41b30efd3a0a85549a8d2b26664fd251
kaoru inoue <kaoru-inoue@aoi-zemi.com>
2020-04-12 16:11:54 +0900

    OpenRC から systemd への変更

--- 2nd_stage_setup.bash
+++ 2nd_stage_setup.bash
@@ -23,21 +23,25 @@ export CCACHE_DIR=~/.ccache
 # export set CC='ccache gcc'
 EOS
 
+# システムのプロファイルは17 systemd にする
+profile=$(eselect profile list |grep '17.0/systemd' | awk '{print $1}' | sed -e 's/\[//' -e 's/\]//')
+eselect profile set ${profile}
+# ロケールは ja_JP.utf8
+eselect_locale=$(eselect locale list |grep 'ja_JP.utf8' | awk '{print $1}' | sed -e 's/\[//' -e 's/\]//')
+eselect locale set ${eselect_locale}
+
 emerge -v gentoo-sources genkernel dev-util/ccache
 emerge -v net-misc/dhcpcd net-misc/openssh tmux vim pciutils sudo metalog fcron mlocate grub
 LANG='C' useradd -m -G users,portage,wheel -s /bin/bash inoue
 echo 'add password'
 LANG='C' passwd inoue
 
-eselect editor 3
+eselect editor set 3
 visudo
 
 cd /usr/src/linux
 make menuconfig
-make -j6 && make modules_install && make install
-
-wait
-genkernel --install initramfs && grub-install /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
+make -j6 && make modules_install && make install && genkernel --install initramfs && grub-install /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
 
 e2label /dev/sda2 boot
 e2label /dev/sda4 root
@@ -52,12 +56,6 @@ LABEL=boot    /boot       ext4    noauto,noatime  1 2
 LABEL=root    /           ext4    noatime         0 1
 LABEL=swap    none        swap    sw              0 0
 EOS
-
-# システムのプロファイルは17にする
-profile=$(eselect profile list |grep '17.0 ' | awk '{print $1}' | sed -e 's/\[//' -e 's/\]//')
-eselect profile set ${profile}
-
-eselect locale set 3
 
 rc-update add sshd default
 rc-update add metalog default
